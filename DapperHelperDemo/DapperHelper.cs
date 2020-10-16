945d83f5880e421827174b82a1bf96944fb2c2b5
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2020-10-16 08:57:02 -0700

    Reset DapperHelper to minimum working demo for INSERTs.

--- DapperHelperDemo/DapperHelper.cs
+++ DapperHelperDemo/DapperHelper.cs
@@ -12,7 +12,6 @@ namespace DapperHelperDemo
     public class DapperHelper
     {
         private readonly SortedDictionary<string, object> _collection = new SortedDictionary<string, object>();
-        private readonly SortedDictionary<string, object> _conditions = new SortedDictionary<string, object>();
 
         private readonly string _tableName;
 
@@ -20,8 +19,6 @@ namespace DapperHelperDemo
         {
             _tableName = tableName;
         }
-
-        public bool HasChanges => _collection.Keys.Count > 0;
 
         /// <summary>
         /// Adds the supplied parameterName to value mapping to a collection
@@ -38,99 +35,6 @@ namespace DapperHelperDemo
             _collection.Add(parameterName, value);
         }
 
-        /// <summary>
-        /// Method to add variables that will not be affected by insert or update
-        /// operations but are necessary for use in the where clause
-        /// </summary>
-        /// <param name="parameterName"></param>
-        /// <param name="value"></param>
-        public void AddCondition(string parameterName, object value)
-        {
-            if (_collection.ContainsKey(parameterName))
-                throw new DuplicateNameException("This field was already declared");
-
-            _conditions.Add(parameterName, value);
-        }
-
-        /// <summary>
-        /// Checks if the supplied bool exists prior to adding it to the collection of parameters
-        /// </summary>
-        /// <param name="parameterName"></param>
-        /// <param name="value"></param>
-        /// <returns>True if Added</returns>
-        public bool SafeAdd(string parameterName, bool? value)
-        {
-            if (string.IsNullOrEmpty(value.ToString())) return false;
-
-            Add(parameterName, value);
-            return true;
-        }
-
-        public bool SafeAdd<TEnum>(string parameterName, string value) where TEnum : struct
-        {
-            if (Enum.TryParse<TEnum>(value, out var enumValue))
-            {
-                Add(parameterName, enumValue);
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool SafeAdd(string parameterName, int? value)
-        {
-            if (!value.HasValue) return false;
-
-            Add(parameterName, value);
-            return true;
-        }
-
-        public bool SafeAdd(string parameterName, long? value)
-        {
-            if (!value.HasValue) return false;
-
-            Add(parameterName, value);
-            return true;
-        }
-
-        /// <summary>
-        /// Checks if the supplied DateTime exists prior to adding it to the collection of parameters
-        /// </summary>
-        /// <param name="parameterName"></param>
-        /// <param name="value"></param>
-        /// <returns>True if Added</returns>
-        public bool SafeAdd(string parameterName, DateTime? value)
-        {
-            if (value > Convert.ToDateTime("01/01/1900"))
-            {
-                Add(parameterName, value);
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool SafeAdd(string parameterName, decimal? value)
-        {
-            if (!value.HasValue) return false;
-
-            Add(parameterName, value);
-            return true;
-        }
-
-        /// <summary>
-        /// Checks if the supplied string is not null or empty prior to adding it to the collection of parameters
-        /// </summary>
-        /// <param name="parameterName"></param>
-        /// <param name="value"></param>
-        /// <returns>True if Added</returns>
-        public bool SafeAdd(string parameterName, string value)
-        {
-            if (string.IsNullOrEmpty(value)) return false;
-
-            Add(parameterName, value);
-            return true;
-        }
 
         /// <summary>
         /// Creates an insert statement for a single record that returns the bigint 
@@ -167,11 +71,6 @@ namespace DapperHelperDemo
                 foreach (var parameterName in _collection.Keys)
                 {
                     parms.Add(parameterName, _collection[parameterName]);
-                }
-
-                foreach (var parameterName in _conditions.Keys)
-                {
-                    parms.Add(parameterName, _conditions[parameterName]);
                 }
                 return parms;
             }
